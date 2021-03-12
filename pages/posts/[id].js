f327f55f6c44ed58bc85fda5045817b94e7c8b85
Shilpi Jain <shilpi@carandbike.com>
2021-03-12 17:36:23 +0530

    Static & Server Side Rendering of Page using API

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,53 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const posts = [
-    {
-        id: 0,
-        title: "Zero"
-    },
-    {
-        id: 1,
-        title: "One"
-    },
-    {
-        id: 2,
-        title: "Two"
-    },
-    {
-        id: 3,
-        title: "Three"
-    },
-];
+// const posts = [
+//     {
+//         id: 0,
+//         title: "Zero"
+//     },
+//     {
+//         id: 1,
+//         title: "One"
+//     },
+//     {
+//         id: 2,
+//         title: "Two"
+//     },
+//     {
+//         id: 3,
+//         title: "Three"
+//     },
+// ];
 
-// function getPostDataById (id) {
-//     postData.filter(({id}) => {
-//         debugger;
-//         return(id != id)
-//     });
+// function getPostDataById(id) {
+//     for (let i = 0; i < posts.length; i++) {
+//       if (posts[i].id === parseInt(id)) {
+//         return posts[i];
+//       }
+//     }
 // }
-function getPostDataById(id) {
-    for (let i = 0; i < posts.length; i++) {
-      if (posts[i].id === parseInt(id)) {
-        return posts[i];
-      }
-    }
-}
 
-// export default function Post  ({id}) {
-//     debugger;
-//     const postData = getPostDataById(id);
-//     console.log(postData)
-//     return (
-//         <div>
-//             {postData.id}
-//             {postData.title}
-//         </div>
-//     )
-// }
-export default function Post({ id }) {
-    const postData = getPostDataById(id);
-  
+export default function Post({ postData }) {
+    // const postData = getPostDataById(id);
+    console.log(`posts,,,${JSON.stringify(postData)}`)
     return (
       <div>
         <h2>{postData.title}</h2>
@@ -55,14 +38,21 @@ export default function Post({ id }) {
       </div>
     );
 }
-// Post.getInitialProps = async ({query}) => {
-//     debugger;
-//     const {id} = query;
-//     console.log(id)
-//     return {id};
-// }
-Post.getInitialProps = async ({ query }) => {
-    const { id } = query;
+
+// Post.getInitialProps = async ({ query }) => {
+//     const { id } = query;
   
-    return { id };
-};
+//     return { id };
+// };
+
+export async function getServerSideProps({query}){
+    const { id } = query;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const postData = await res.json();
+
+    return {
+        props : {
+            postData
+        },
+    };
+}
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import Link from 'next/link'
 
-const posts = [
-    {
-        id: 0,
-        title: "Zero"
-    },
-    {
-        id: 1,
-        title: "One"
-    },
-    {
-        id: 2,
-        title: "Two"
-    },
-    {
-        id: 3,
-        title: "Three"
-    },
-];
+// const posts = [
+//     {
+//         id: 0,
+//         title: "Zero"
+//     },
+//     {
+//         id: 1,
+//         title: "One"
+//     },
+//     {
+//         id: 2,
+//         title: "Two"
+//     },
+//     {
+//         id: 3,
+//         title: "Three"
+//     },
+// ];
 
-const Posts = () => {
+const Posts = ({posts}) => {
+    // console.log(`posts,,,${JSON.stringify(posts)}`)
     return (
         <ul>
             {
@@ -37,5 +38,24 @@ const Posts = () => {
     )
 }
 
+// export async function getStaticProps(){
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     const posts = await res.json();
 
+//     return {
+//         props : {
+//             posts
+//         },
+//     };
+// }
+export async function getServerSideProps(){
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await res.json();
+
+    return {
+        props : {
+            posts
+        },
+    };
+}
 export default Posts;
